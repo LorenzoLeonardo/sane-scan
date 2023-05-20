136c5c49ee0995df274a4db3f5054dfdedc9b58c
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-05-20 11:14:53 +0800

    Parameterized local_only for get_devices()
    This will make it dynamic.

--- src/lib.rs
+++ src/lib.rs
@@ -60,10 +60,14 @@ impl Sane {
 		Self::init(version_code)
 	}
 
-	pub fn get_devices(&self) -> Result<Vec<Device>> {
+	pub fn get_devices(&self, is_local: bool) -> Result<Vec<Device>> {
+		let mut local_only: i32 = 0;
+		if is_local {
+			local_only = 1;
+		}
 		let mut device_list: *mut *const sys::Device = std::ptr::null_mut();
 		let status: sys::Status =
-			unsafe { sys::sane_get_devices(&mut device_list as *mut *mut *const sys::Device, 1) };
+			unsafe { sys::sane_get_devices(&mut device_list as *mut *mut *const sys::Device, local_only) };
 		if status != sys::Status::Good {
 			return Err(Error(status));
 		}
